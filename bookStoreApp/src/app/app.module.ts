2332dab3678b88e814210d629d4a007df07970f6
deepro__ <mueezrz@gmail.com>
2023-08-30 00:50:52 +0600

    Angylar material, toolbar, button, material module

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     // PublicModule,
     SharedModule,
     BrowserAnimationsModule
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
