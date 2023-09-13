b1cb8a0b5c01f674e482b1ce5923839150405daa
deepro__ <mueezrz@gmail.com>
2023-09-14 00:31:04 +0600

    Creating reactive forms in add books

--- bookStoreApp/src/app/books/books.module.ts
+++ bookStoreApp/src/app/books/books.module.ts
@@ -9,7 +9,8 @@ import { SharedModule } from '../shared/shared.module';
 import { RecentBooksComponent } from './components/recent-books/recent-books.component';
 import { BookCardComponent } from './components/book-card/book-card.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddBookReactiveComponent } from './components/add-book-reactive/add-book-reactive.component';
 
 
 
@@ -20,14 +21,16 @@ import { FormsModule } from '@angular/forms';
     BooksComponent,
     RecentBooksComponent,
     BookCardComponent,
-    AddBookComponent
+    AddBookComponent,
+    AddBookReactiveComponent
 
   ],
   imports: [
     CommonModule,
     PublicRoutingModule,
     SharedModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class BooksModule { }
--- bookStoreApp/src/app/books/components/add-book/add-book.component.ts
+++ bookStoreApp/src/app/books/components/add-book/add-book.component.ts
@@ -8,6 +8,7 @@ import {
 import { BookService } from '../../services/book.service';
 import { BookModel } from '../../models/book.model';
 import { PriceModel } from '../../models/price.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-book',
@@ -15,40 +16,34 @@ import { PriceModel } from '../../models/price.model';
   styleUrls: ['./add-book.component.scss'],
 })
 export class AddBookComponent implements OnInit {
+  @ViewChild('myForm') myForm: NgForm;
 
   prices: any[] = [
-    {value: '100', viewValue: '$ 100'},
-    {value: '200', viewValue: '$ 200'},
-    {value: '300', viewValue: '$ 300'},
+    { value: '100', viewValue: '$ 100' },
+    { value: '200', viewValue: '$ 200' },
+    { value: '300', viewValue: '$ 300' },
   ];
 
   public model: BookModel;
-  constructor(private _bookService: BookService){}
+  constructor(private _bookService: BookService) {}
   ngOnInit(): void {
     this.model = new BookModel();
-    this.model.title = 'Book';
+    //this.model.title = 'Book';
     this.model.totalPages = 100;
     this.model.price = {
       value: 100,
-      currency: 'USD'
+      currency: 'USD',
     };
     this.model.isPublished = true;
   }
 
-
-
   saveBook(): void {
-     console.log(this.model);
-    // const book: BookModel = new BookModel();
-    // book.title = value.title;
-    // book.author = value.author;
-    // book.totalPages = value.pages;
-    // book.price = {
-    //   currency: "$",
-    //   value: value.price
-    // };
-    // book.isPublished = value.isPublished;
-    // book.publishedOn = value.date;
-    this._bookService.addBook(this.model);
+    console.log(this.model);
+
+    if (this.myForm.valid) {
+      this._bookService.addBook(this.model);
+    } else {
+      alert('form Invalid');
+    }
   }
 }
